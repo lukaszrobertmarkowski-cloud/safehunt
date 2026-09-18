c39218c84d2265f8710cfa45f4b9d6dfff57618e
lukaszrobertmarkowski-cloud <lukasz.robert.markowski@gmail.com>
2026-09-18 22:31:55 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='hunter-distance-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
