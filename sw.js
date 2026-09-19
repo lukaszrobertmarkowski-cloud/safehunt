5d0a2ad2b7a20de029d6577b354388714ee50f5c
lukaszrobertmarkowski-cloud <lukasz.robert.markowski@gmail.com>
2026-09-19 21:22:56 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='safe-hunt-v4';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest'])))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()]))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
